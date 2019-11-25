7c6f66bbf69555a0c3a94c6c50288235816bf7f8
olukayode <jt.olukayode@yahoo.com>
2019-11-26 00:41:49 +0100

    designed home screen

---
+++ lib/widgets/activity-card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ActivityOverview extends StatelessWidget {
+  final String entryTitle;
+  final int totalEntry;
+  final int completedEntry;
+
+  const ActivityOverview(
+      {Key key, this.entryTitle, this.totalEntry, this.completedEntry})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double scrHeight = MediaQuery.of(context).size.height;
+    double scrWudth = MediaQuery.of(context).size.width;
+    return Container(
+      width: 0.40 * scrWudth,
+      padding: EdgeInsets.all(8.0),
+      child: Card(shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(17.0))),
+        // margin: EdgeInsets.only(right: 10.0),
+        elevation: 8.0,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                entryTitle,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+              RichText(
+                text: TextSpan(children: <TextSpan>[
+                  TextSpan(
+                      text: completedEntry.toString(),
+                      style: TextStyle(color: Colors.black)),
+                  TextSpan(text: "/", style: TextStyle(color: Colors.black)),
+                  TextSpan(
+                      text: totalEntry.toString(),
+                      style: TextStyle(color: Colors.black))
+                ]),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
