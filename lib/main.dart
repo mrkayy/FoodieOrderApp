ff79cf5a3ea7f703c4fe31491b79c8c401fca95b
olukayode <jt.olukayode@yahoo.com>
2020-02-22 15:58:55 +0100

    completed splashscreen design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'screens/dashboard.dart';
+import './screens/dashboard.dart';
+import './screens/splash_screen.dart';
 
-// import 'constants.dart'; 
+// import 'constants.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,103 +11,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'Foodie',
       theme: ThemeData(
-        // fontFamily: GoogleFonts.comfortaa(),
-        // brightness: Brightness.dark,
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.blue,
       ),
       initialRoute: SplashScreen.id,
       routes: <String, WidgetBuilder>{
         SplashScreen.id: (BuildContext context) => SplashScreen(),
         Dashboard.id: (BuildContext context) => Dashboard(),
       },
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  static String id = "initRoute";
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  void delayScreen() {
-    Future.delayed(Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (BuildContext context) => Dashboard()));
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    delayScreen();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final scrData = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Stack(
-        children: <Widget>[
-          // Container(
-          //   color: Colors.deepOrange,
-          //   width: double.infinity,
-          //   height: scrData.height,
-          // ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              // color: Colors.black,
-              width: double.infinity,
-              height: scrData.height, //0.75 * scrData.height,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.red[900].withOpacity(1),
-                    Colors.red[900].withOpacity(0.9),
-                    Colors.red[900].withOpacity(0.8),
-                    Colors.red[900].withOpacity(0.7),
-                    Colors.red[900].withOpacity(0.6),
-                    Colors.red[800].withOpacity(0.5),
-                    Colors.red[800].withOpacity(0.4),
-                    // Colors.red[800].withOpacity(0.3),
-                    // Colors.red[800].withOpacity(0.2),
-                    Colors.red[800].withOpacity(0.1),
-                    Colors.red[800].withOpacity(0.05),
-                    Colors.red[800].withOpacity(0.025),
-                  ],
-                ),
-              ),
-              // child: Text("data"),
-            ),
-          ),
-          Positioned(
-            bottom: 0.25 * scrData.height,
-            left: 0.0,
-            right: 0.0,
-            child: Center(
-              child: Text(
-                'Foodie',
-                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
-                style: GoogleFonts.pacifico(
-                  color: Colors.white,
-                  fontSize: 42.0,
-                ),
-              ),
-            ),
-          ),
-          //TODO: add animation for login for
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './dashboard.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  static String id = "initRoute";
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void delayScreen() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) => Dashboard()));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    delayScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scrData = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: double.infinity, //0.75 * scrData.height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.blue[900].withOpacity(1),
+                    Colors.blue[900].withOpacity(0.9),
+                    Colors.blue[900].withOpacity(0.8),
+                    Colors.blue[900].withOpacity(0.7),
+                    Colors.blue[900].withOpacity(0.6),
+                    Colors.blue[800].withOpacity(0.5),
+                    Colors.blue[800].withOpacity(0.4),
+                    Colors.blue[800].withOpacity(0.1),
+                    Colors.blue[800].withOpacity(0.05),
+                    Colors.blue[800].withOpacity(0.025),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.35 * scrData.height,
+            child: Material(
+              color: Colors.white,
+              shape: CircleBorder(),
+              elevation: 5.0,
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Image.asset(
+                  "assets/images/applogo.png",
+                  height: 110.0,
+                  width: 110.0,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.25 * scrData.height,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Foodie',
+                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
+                style: GoogleFonts.pacifico(
+                  color: Colors.white,
+                  fontSize: 42.0,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.02 * scrData.height,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                'powered by: Cedarcliff Services',
+                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
+                style: GoogleFonts.workSans(
+                  letterSpacing: 0.8,
+                  fontSize: 10.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
