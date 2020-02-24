78e6861f528b745b7aaed72ee77a0be4f89cfa3d
olukayode <jt.olukayode@yahoo.com>
2020-02-24 20:07:40 +0100

    refactored splash screen & signin screen

--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -14,10 +14,113 @@ class SigninPage extends StatefulWidget {
 class _SigninPageState extends State<SigninPage> {
   @override
   Widget build(BuildContext context) {
-  final scrData = MediaQuery.of(context).size;
+    final scrData = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Stack(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity, //0.75 * scrData.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.blue[900].withOpacity(1),
+              Colors.blue[900].withOpacity(0.9),
+              Colors.blue[900].withOpacity(0.8),
+              Colors.blue[900].withOpacity(0.7),
+              Colors.blue[900].withOpacity(0.6),
+              Colors.blue[800].withOpacity(0.5),
+              Colors.blue[800].withOpacity(0.4),
+              Colors.blue[800].withOpacity(0.1),
+              Colors.blue[800].withOpacity(0.05),
+              Colors.blue[800].withOpacity(0.025),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(top: 220),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Material(
+                      color: Colors.white,
+                      shape: CircleBorder(),
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Image.asset(
+                          "assets/images/applogo.png",
+                          height: 100.0 / 1.4,
+                          width: 100.0 / 1.4,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Foodie',
+                      // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
+                      style: GoogleFonts.pacifico(
+                        color: Colors.white,
+                        fontSize: 42.0 / 1.4,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                // padding: const EdgeInsets.only(bottom: 120),
+                margin: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Login",
+                            style: GoogleFonts.workSans(
+                              fontSize: 28.0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "\nWelcome back ${widget.userName}",
+                            style: GoogleFonts.workSans(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      textInputAction: TextInputAction.go,
+                    ),
+                    SizedBox(height: 0.05 * scrData.height),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0)),
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                      minWidth: 0.5 * scrData.width,
+                      color: Colors.white,
+                      child: Text("Login"),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      /*
+      Stack(
         children: <Widget>[
           Positioned(
             child: Container(
@@ -47,57 +150,78 @@ class _SigninPageState extends State<SigninPage> {
           Positioned(
             left: 0.0,
             right: 0.0,
-            bottom: 0.5 * scrData.height,
+            top: 0.3 * scrData.height,
             child: Material(
               color: Colors.white,
               shape: CircleBorder(),
               elevation: 5.0,
               child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Image.asset(
-                  "assets/images/applogo.png",
-                  height: 110.0,
-                  width: 110.0,
+                padding: const EdgeInsets.all(18.0 / 1.4),
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/applogo.png",
+                      height: 110.0 / 1.4,
+                      width: 110.0 / 1.4,
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.25 * scrData.height,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'Foodie',
-                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
-                style: GoogleFonts.pacifico(
-                  color: Colors.white,
-                  fontSize: 42.0,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.02 * scrData.height,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'powered by: Cedarcliff Services',
-                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
-                style: GoogleFonts.workSans(
-                  letterSpacing: 0.8,
-                  fontSize: 10.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
+          // Positioned(
+          //   left: 0.0,
+          //   right: 0.0,
+          //   top: 0.45 * scrData.height,
+          //   child: Container(
+          //     margin: const EdgeInsets.symmetric(horizontal: 32.0),
+          //     // color: Colors.red,
+          //     alignment: Alignment.center,
+          //     child: Column(
+          //       children: <Widget>[
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          // Positioned(
+          //   left: 0.0,
+          //   right: 0.0,
+          //   bottom: 0.3 * scrData.height,
+          //   child: Container(
+          //     margin: const EdgeInsets.symmetric(horizontal: 32.0),
+          //     // color: Colors.red,
+          //     alignment: Alignment.center,
+          //     child: Column(
+          //       children: <Widget>[
+          //         RichText(
+          //           textAlign: TextAlign.center,
+          //           text: TextSpan(
+          //             children: [
+          //               TextSpan(
+          //                 text: "Login",
+          //                 style: GoogleFonts.workSans(
+          //                   fontSize: 28.0,
+          //                 ),
+          //               ),
+          //               TextSpan(
+          //                 text: "\nWelcome back ${widget.userName}",
+          //                 style: GoogleFonts.workSans(
+          //                   fontSize: 14.0,
+          //                 ),
+          //               ),
+          //             ],
+          //           ),
+          //         ),
+          //         SizedBox(height: 10.0),
+          //         TextField(),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          //
         ],
       ),
+      */
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,10 +16,9 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _appHasUser() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool prefValue = true;//prefs.getBool("has_user");
+    bool prefValue = true; //prefs.getBool("has_user");
 
     if (prefValue != null || prefValue != false) {
-      print(prefValue);
       //get username from storage
       userName = prefs.getString("user_name");
       //application already has a user
@@ -57,86 +56,80 @@ class _SplashScreenState extends State<SplashScreen> {
     final scrData = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            child: Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: double.infinity, //0.75 * scrData.height,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.blue[900].withOpacity(1),
-                    Colors.blue[900].withOpacity(0.9),
-                    Colors.blue[900].withOpacity(0.8),
-                    Colors.blue[900].withOpacity(0.7),
-                    Colors.blue[900].withOpacity(0.6),
-                    Colors.blue[800].withOpacity(0.5),
-                    Colors.blue[800].withOpacity(0.4),
-                    Colors.blue[800].withOpacity(0.1),
-                    Colors.blue[800].withOpacity(0.05),
-                    Colors.blue[800].withOpacity(0.025),
+      body: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: double.infinity, //0.75 * scrData.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.blue[900].withOpacity(1),
+              Colors.blue[900].withOpacity(0.9),
+              Colors.blue[900].withOpacity(0.8),
+              Colors.blue[900].withOpacity(0.7),
+              Colors.blue[900].withOpacity(0.6),
+              Colors.blue[800].withOpacity(0.5),
+              Colors.blue[800].withOpacity(0.4),
+              Colors.blue[800].withOpacity(0.1),
+              Colors.blue[800].withOpacity(0.05),
+              Colors.blue[800].withOpacity(0.025),
+            ],
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 15,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Material(
+                      color: Colors.white,
+                      shape: CircleBorder(),
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Image.asset(
+                          "assets/images/applogo.png",
+                          height: 115.0/1.4,
+                          width: 115.0/1.4,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Foodie',
+                      // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
+                      style: GoogleFonts.pacifico(
+                        color: Colors.white,
+                        fontSize: 42.0,
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.35 * scrData.height,
-            child: Material(
-              color: Colors.white,
-              shape: CircleBorder(),
-              elevation: 5.0,
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Image.asset(
-                  "assets/images/applogo.png",
-                  height: 110.0,
-                  width: 110.0,
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(bottom: 37.0),
+                alignment: Alignment.center,
+                child: Text(
+                  'powered by: Cedarcliff Services',
+                  // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
+                  style: GoogleFonts.workSans(
+                    letterSpacing: 0.8,
+                    fontSize: 10.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.25 * scrData.height,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'Foodie',
-                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
-                style: GoogleFonts.pacifico(
-                  color: Colors.white,
-                  fontSize: 42.0,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.02 * scrData.height,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                'powered by: Cedarcliff Services',
-                // style: kHeadingText.copyWith(color: Colors.white, fontSize: 36.0),
-                style: GoogleFonts.workSans(
-                  letterSpacing: 0.8,
-                  fontSize: 10.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
